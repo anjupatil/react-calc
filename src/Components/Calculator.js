53bfc585f7725a9c5fbb18b2381a11d623d10f2e
anjupatil <anjupatil42@gmail.com>
2023-05-13 14:42:44 +0530

    Update Calculator component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -7,12 +7,14 @@ const Calculator = () => {
   const [num1, setNum1] = useState('');
   const [num2, setNum2] = useState('');
   const [result, setResult] = useState('');
+  const [success,setSuccess] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleAddition = () => {
     if (validateInput()) {
       const sum = parseFloat(num1) + parseFloat(num2);
       setResult(sum.toString());
+      setSuccess("Your result is shown above");
       setErrorMessage('');
     }
   };
@@ -21,6 +23,7 @@ const Calculator = () => {
     if (validateInput()) {
       const difference = parseFloat(num1) - parseFloat(num2);
       setResult(difference.toString());
+      setSuccess("Your result is shown above");
       setErrorMessage('');
     }
   };
@@ -29,6 +32,7 @@ const Calculator = () => {
     if (validateInput()) {
       const product = parseFloat(num1) * parseFloat(num2);
       setResult(product.toString());
+      setSuccess("Your result is shown above");
       setErrorMessage('');
     }
   };
@@ -38,25 +42,43 @@ const Calculator = () => {
       if (parseFloat(num2) === 0) {
         setErrorMessage('Error: Division by zero');
         setResult('');
+        setSuccess('');
       } else {
         const quotient = parseFloat(num1) / parseFloat(num2);
         setResult(quotient.toString());
+        setSuccess("Your result is shown above");
         setErrorMessage('');
       }
     }
   };
 
   const validateInput = () => {
-    if (num1 === '' || num2 === '') {
-      setErrorMessage('Error: Please enter two numbers');
+    if(num1 === ''&& num2 === ''){
+         setErrorMessage('Error: Enter two numbers');
+         setResult('');
+         setSuccess('');
+         return false;
+   
+       }
+    else if (num1 === '') {
+      setErrorMessage('Error: Num 1 cannot be empty');
       setResult('');
+      setSuccess('');
       return false;
+    }
+    else if(num2 === ''){
+      setErrorMessage('Error: Num 2 cannot be empty');
+      setResult('');
+      setSuccess('');
+      return false;
+
     }
 
     const numberRegex = /^-?\d*\.?\d*$/;
     if (!numberRegex.test(num1) || !numberRegex.test(num2)) {
       setErrorMessage('Error: Please enter valid numbers');
       setResult('');
+      setSuccess('');
       return false;
     }
 
@@ -78,6 +100,7 @@ const Calculator = () => {
 
       {errorMessage && <div className="error">{errorMessage}</div>}
       {result && <div className="success">Result: {result}</div>}
+      {success && <div className="success">{success}</div>}
     </div>
   );
 };
